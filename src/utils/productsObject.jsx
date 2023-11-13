afd90c1020fce1214dbd1ba183b754832eac8fe7
Muhammad Selim <muhamadselim70@gmail.com>
2023-11-13 17:13:36 +0200

    Refactored code to improve performance and readability.

--- src/utils/productsObject.jsx
+++ src/utils/productsObject.jsx
@@ -1,88 +1,36 @@
-import laMarzocco from "../assets/images/products/brands/laMarzocco.webp";
-import rocket from "../assets/images/products/brands/rocket.webp";
-import mahlkonig from "../assets/images/products/brands/mahlkonig.webp";
-import anfim from "../assets/images/products/brands/anfim.webp";
-import fiorenzato from "../assets/images/products/brands/fiorenzato.webp";
-import eurekaORO from "../assets/images/products/brands/eurekaORO.webp";
-import quamar from "../assets/images/products/brands/quamar.webp";
-import baratza from "../assets/images/products/brands/baratza.webp";
-import irhea from "../assets/images/products/brands/irhea.webp";
-import fetoc from "../assets/images/products/brands/fetoc.webp";
-import unox from "../assets/images/products/brands/unox.webp";
-import acaia from "../assets/images/products/brands/acaia.webp";
-import blander from "../assets/images/products/brands/rocket.webp";
-import iceMaker from "../assets/images/products/brands/rocket.webp";
-import bwt from "../assets/images/products/brands/rocket.webp";
-
-const brands = [
-  {
-    link: "https://espressostation.co/ar/La-marzocco/brand-1999061827",
-    title: "La Marzocco",
-    logo: laMarzocco,
-  },
-  {
-    link: "https://espressostation.co/ar/ROCKET-ESPRESSO-MILANO/brand-151121469",
-    title: "Rocket",
-    logo: rocket,
-  },
-  {
-    link: "https://espressostation.co/ar/%D9%85%D8%A7%D9%84%D9%83%D9%88%D9%86%D9%8A%D9%82-Mahlkonig/brand-73827397",
-    title: "Mahlkonig",
-    logo: mahlkonig,
-  },
-  {
-    link: "https://espressostation.co/ar/anifm/brand-1979945863",
-    title: "Anfim",
-    logo: anfim,
-  },
-  {
-    link: "https://espressostation.co/ar/Fiorenzato/brand-2120200796",
-    title: "FIORENZATO",
-    logo: fiorenzato,
-  },
-  {
-    link: "https://espressostation.co/ar/eureka-oro/brand-2099428466",
-    title: "Eureka ORO",
-    logo: eurekaORO,
-  },
-  {
-    link: "https://espressostation.co/ar/QUAMAR-%D9%83%D9%88%D8%A7%D9%85%D8%A7%D8%B1/brand-8700580839",
-    title: "Quamar",
-    logo: quamar,
-  },
-  {
-    link: "https://espressostation.co/ar/baratza/brand-1797631004",
-    title: "Baratza",
-    logo: baratza,
-  },
-  {
-    link: "https://espressostation.co/ar/irhea/brand-661493699",
-    title: "IRHEA",
-    logo: irhea,
-  },
-  {
-    link: "https://espressostation.co/ar/%D9%81%D8%AA%D9%83%D9%88-fetco/brand-877564021",
-    title: "FETCO",
-    logo: fetoc,
-  },
-  {
-    link: "https://espressostation.co/ar/bwt/brand-1488143995",
-    title: "BWT",
-    logo: bwt,
-  },
-  { link: "#", title: "ICE MAKER", logo: iceMaker },
-  {
-    link: "https://espressostation.co/ar/OUNX/brand-559911320",
-    title: "UNOX",
-    logo: unox,
-  },
-  { link: "#", title: "Blander", logo: blander },
-  {
-    link: "https://espressostation.co/ar/%D9%81%D8%AA%D9%83%D9%88-fetco/brand-877564021",
-    title: "Acaia",
-    logo: acaia,
-  },
-];
+import { brands } from "./productBrand";
+import _kb90 from "../assets/images/products/brand-products/kb90.webp";
+import _11 from "../assets/images/products/brand-products/11.webp";
+import _3 from "../assets/images/products/brand-products/3.webp";
+import _5i from "../assets/images/products/brand-products/5i.webp";
+import _9i from "../assets/images/products/brand-products/9i.webp";
+import _12 from "../assets/images/products/brand-products/12.webp";
+import _AV from "../assets/images/products/brand-products/AV.webp";
+import _mp from "../assets/images/products/brand-products/mp.webp";
+import _ap from "../assets/images/products/brand-products/ap.webp";
+import _58 from "../assets/images/products/brand-products/58.webp";
+import _r9 from "../assets/images/products/brand-products/r9.webp";
+import _T1 from "../assets/images/products/brand-products/T1.webp";
+import _T2 from "../assets/images/products/brand-products/T2.webp";
+import _T3 from "../assets/images/products/brand-products/T3.webp";
+import _T4 from "../assets/images/products/brand-products/T4.webp";
+import _T5 from "../assets/images/products/brand-products/T5.webp";
+import _T7 from "../assets/images/products/brand-products/T7.webp";
+import _T6 from "../assets/images/products/brand-products/T6.webp";
+import _T8 from "../assets/images/products/brand-products/T8.webp";
+import _T10 from "../assets/images/products/brand-products/T10.webp";
+import _T9 from "../assets/images/products/brand-products/T9.webp";
+import _3i from "../assets/images/products/brand-products/3i.webp";
+import _2i from "../assets/images/products/brand-products/2i.webp";
+import _4i from "../assets/images/products/brand-products/4i.webp";
+import _5 from "../assets/images/products/brand-products/5.webp";
+import _1 from "../assets/images/products/brand-products/1.webp";
+import _pic18 from "../assets/images/products/brand-products/pic 18.webp";
+import _8i from "../assets/images/products/brand-products/8i.webp";
+import _7i from "../assets/images/products/brand-products/7i.webp";
+import _6i from "../assets/images/products/brand-products/6i.webp";
+import _pic27 from "../assets/images/products/brand-products/pic 27.webp";
+import _pr from "../assets/images/products/brand-products/pr.webp";
 
 export const productList = {
   equipmentCommercial: {
@@ -93,17 +41,17 @@ export const productList = {
           {
             title: "La Marzocco KB90",
             link: "https://espressostation.co/ar/%D9%84%D8%A7%D9%85%D8%A7%D8%B1%D8%B2%D9%88%D9%83%D9%88-KB90/p846134749",
-            imgSrc: "kb90.png",
+            imgSrc: _kb90,
           },
           {
             title: "LA Marzocco Strada AV 2 GROUPS",
             link: "https://espressostation.co/ar/%D9%84%D8%A7%D9%85%D8%A7%D8%B1%D8%B2%D9%88%D9%83%D9%88-%D8%A7%D8%B3%D8%AA%D8%B1%D8%A7%D8%AF%D8%A7/p878652110",
-            imgSrc: "11.webp",
+            imgSrc: _11,
           },
           {
             title: "LA Marzocco GB5 X 3GROUP",
             link: "https://espressostation.co/ar/%D9%84%D8%A7%D9%85%D8%A7%D8%B1%D8%B2%D9%88%D9%83%D9%88-%DA%86%D9%8A-%D8%A8%D9%8A-5/p602200310",
-            imgSrc: "3.webp",
+            imgSrc: _3,
           },
         ],
       },
@@ -113,17 +61,17 @@ export const productList = {
           {
             title: "( SOTTO BANCO ) ",
             link: "https://espressostation.co/ar/%D8%A2%D9%84%D8%A9-%D8%A7%D9%84%D8%A7%D8%B3%D8%A8%D8%B1%D9%8A%D8%B3%D9%88-sotto-banco-%D8%A8%D9%85%D8%AC%D9%85%D9%88%D8%B9%D8%AA%D9%8A%D9%86-%D9%85%D9%86-%D8%B1%D9%88%D9%83%D9%8A%D8%AA/p1266444965",
-            imgSrc: "5i.webp",
+            imgSrc: _5i,
           },
           {
             title: "Rocket Espresso R9V",
             link: "https://espressostation.co/ar/%D8%B1%D9%88%D9%83%D9%8A%D8%AA-R9V/p9358275",
-            imgSrc: "9i.webp",
+            imgSrc: _9i,
           },
           {
             title: "RE Dobia",
             link: "https://espressostation.co/ar/%D8%AF%D9%88%D8%A8%D9%8A%D8%A7-%D8%B1%D9%88%D9%83%D9%8A%D8%AA/p1762006472",
-            imgSrc: "12.webp",
+            imgSrc: _12,
           },
         ],
       },
@@ -137,12 +85,12 @@ export const productList = {
           {
             title: "LA Marzocco GS3 MP",
             link: "https://espressostation.co/ar/gs3-mp-la-marzocco/p1691845633",
-            imgSrc: "AV.png",
+            imgSrc: _AV,
           },
           {
             title: "LA Marzocco GS3 AV",
             link: "https://espressostation.co/ar/gs3-av-la-marzocco/p1978198139",
-            imgSrc: "mp.png",
+            imgSrc: _mp,
           },
         ],
       },
@@ -152,17 +100,17 @@ export const productList = {
           {
             title: "ROCKET APPARTAMENTO",
             link: "https://espressostation.co/ar/%D8%A3%D8%A8%D8%A7%D8%B1%D8%AA%D8%A7%D9%85%D9%86%D8%AA%D9%88-%D8%B1%D9%88%D9%83%D9%8A%D8%AA/p1504220741",
-            imgSrc: "ap.png",
+            imgSrc: _ap,
           },
           {
             title: "Rocket R58 V3",
             link: "https://espressostation.co/ar/%D8%B1%D9%88%D9%83%D9%8A%D8%AA-%D8%A7%D8%B158/p38415075",
-            imgSrc: "58.png",
+            imgSrc: _58,
           },
           {
             title: "Rocket R9 ONE",
             link: "https://espressostation.co/ar/%D8%B1%D9%88%D9%83%D9%8A%D8%AA-%D8%A2%D8%B19-%D9%88%D9%86/p1884260524",
-            imgSrc: "r9.png",
+            imgSrc: _r9,
           },
         ],
       },
@@ -176,17 +124,17 @@ export const productList = {
           {
             title: "MAHLKONIG E80 SUPREME",
             link: "https://espressostation.co/ar/%D9%85%D8%A7%D9%84%D9%83%D9%88%D9%86%D9%8A%D9%82-Mahlkonig/p2119117651",
-            imgSrc: "T1.webp",
+            imgSrc: _T1,
           },
           {
             title: "MAHLKONIG E65 GBW",
             link: "https://espressostation.co/ar/%D9%85%D8%A7%D9%84%D9%83%D9%88%D9%86%D9%8A%D9%82-Mahlkonig/p774351074",
-            imgSrc: "T2.webp",
+            imgSrc: _T2,
           },
           {
             title: "Mahlkonig EK 43S",
             link: "https://espressostation.co/ar/%D9%85%D8%A7%D9%84%D9%83%D9%88%D9%86%D9%8A%D9%82-mahlkoning/p913301691",
-            imgSrc: "T3.webp",
+            imgSrc: _T3,
           },
         ],
       },
@@ -196,7 +144,7 @@ export const productList = {
           {
             title: "Anfim SP II",
             link: "https://espressostation.co/ar/Anfim/p831949597",
-            imgSrc: "T4.webp",
+            imgSrc: _T4,
           },
         ],
       },
@@ -206,7 +154,7 @@ export const productList = {
           {
             title: "F83 XGi PRO",
             link: "https://espressostation.co/ar/%D8%B7%D8%A7%D8%AD%D9%88%D9%86%D8%A9-%D8%A7%D9%84%D9%82%D9%87%D9%88%D8%A9-%D9%81%D9%8A%D9%88%D8%B1%D9%8A%D9%86%D8%B2%D8%A7%D8%AA%D9%88-f83-xgi-pro-%D8%A8%D8%A7%D9%84%D9%88%D8%B2%D9%86/p1957359038",
-            imgSrc: "T5.webp",
+            imgSrc: _T5,
           },
         ],
       },
@@ -220,12 +168,12 @@ export const productList = {
           {
             title: "Single Dose",
             link: "https://espressostation.co/ar/%D9%8A%D9%88%D8%B1%D9%8A%D9%83%D8%A7-%D8%B3%D9%86%D9%82%D9%84-%D8%AF%D9%88%D8%B2/p455487940",
-            imgSrc: "T7.webp",
+            imgSrc: _T7,
           },
           {
             title: "يوريكا هيليوس 75",
             link: "https://espressostation.co/ar/%D9%8A%D9%88%D8%B1%D9%8A%D9%83%D8%A7-%D9%87%D9%8A%D9%84%D9%8A%D9%88%D8%B3/p1876923852",
-            imgSrc: "T6.webp",
+            imgSrc: _T6,
           },
         ],
       },
@@ -235,12 +183,12 @@ export const productList = {
           {
             title: " كوامار Sirio Q",
             link: "https://espressostation.co/ar/%D9%83%D9%88%D8%A7%D9%85%D8%A7%D8%B1-sirio-q/p1044054376",
-            imgSrc: "T8.webp",
+            imgSrc: _T8,
           },
           {
             title: "طاحونة كوامار T48e",
             link: "https://espressostation.co/ar/%D8%B7%D8%A7%D8%AD%D9%88%D9%86%D8%A9-%D9%83%D9%88%D8%A7%D9%85%D8%A7%D8%B1-t48e/p714443634",
-            imgSrc: "T10.webp",
+            imgSrc: _T10,
           },
         ],
       },
@@ -250,7 +198,7 @@ export const productList = {
           {
             title: "Baratza Sette 270Wi",
             link: "https://espressostation.co/ar/%D8%A8%D8%B1%D8%A7%D8%AA%D8%B2%D8%A7-%D8%AF%D8%A8%D9%84%D9%8A%D9%88270/p2065869991",
-            imgSrc: "T9.webp",
+            imgSrc: _T9,
           },
         ],
       },
@@ -264,17 +212,17 @@ export const productList = {
           {
             title: "IRHEA SF ",
             link: "https://espressostation.co/ar/IRHEA/p295080529",
-            imgSrc: "3i.webp",
+            imgSrc: _3i,
           },
           {
             title: "IRHEA M3",
             link: "https://espressostation.co/ar/iRHEA/p708102066",
-            imgSrc: "2i.webp",
+            imgSrc: _2i,
           },
           {
             title: "IRHEA M5",
             link: "https://espressostation.co/ar/iRHEA/p601564349",
-            imgSrc: "4i.webp",
+            imgSrc: _4i,
           },
         ],
       },
@@ -284,7 +232,7 @@ export const productList = {
           {
             title: "CBS-2141XTS",
             link: "https://espressostation.co/ar/%D8%A8%D8%A7%D9%82%D8%A9-%D9%82%D9%87%D9%88%D8%A9-%D8%A7%D9%84%D9%8A%D9%88%D9%85-%D9%84%D9%84%D9%85%D9%82%D8%A7%D9%87%D9%8A/p973793753",
-            imgSrc: "5.webp",
+            imgSrc: _5,
           },
         ],
       },
@@ -299,7 +247,7 @@ export const productList = {
           {
             title: "BWT BESTAQUA 14 ROC COFFEE",
             link: "https://espressostation.co/ar/bwt-bestaqua-14-roc-coffee/p1253813311",
-            imgSrc: "1.webp",
+            imgSrc: _1,
           },
         ],
       },
@@ -313,7 +261,7 @@ export const productList = {
           {
             title: "Ice Maker",
             link: "#",
-            imgSrc: "pic 18.png",
+            imgSrc: _pic18,
           },
         ],
       },
@@ -327,17 +275,17 @@ export const productList = {
           {
             title: "ROSSELLA XFT193",
             link: "https://espressostation.co/ar/%D9%81%D8%B1%D9%86-%D8%AD%D8%B1%D8%A7%D8%B1%D9%8A-%D9%83%D9%87%D8%B1%D8%A8%D8%A7%D8%A6%D9%8A-4-%D8%B5%D9%88%D8%A7%D9%86%D9%8A-%D9%85%D9%86-%D8%A7%D9%88%D9%86%D9%83%D8%B3/p202798243",
-            imgSrc: "8i.webp",
+            imgSrc: _8i,
           },
           {
             title: "ARIANNA XFT133",
             link: "https://espressostation.co/ar/%D9%81%D8%B1%D9%86-%D8%AD%D8%B1%D8%A7%D8%B1%D9%8A-%D9%83%D9%87%D8%B1%D8%A8%D8%A7%D8%A6%D9%8A-4-%D8%B5%D9%88%D8%A7%D9%86%D9%8A-%D9%85%D9%86-%D8%A7%D9%88%D9%86%D9%83%D8%B3/p1692978500",
-            imgSrc: "7i.webp",
+            imgSrc: _7i,
           },
           {
             title: "STEFANIA XFT113",
             link: "https://espressostation.co/ar/%D9%81%D8%B1%D9%86-%D8%AD%D8%B1%D8%A7%D8%B1%D9%8A-%D9%83%D9%87%D8%B1%D8%A8%D8%A7%D8%A6%D9%8A-3-%D8%B5%D9%88%D8%A7%D9%86%D9%8A-%D9%85%D9%86-%D8%A7%D9%88%D9%86%D9%83%D8%B3/p1113400193",
-            imgSrc: "6i.webp",
+            imgSrc: _6i,
           },
         ],
       },
@@ -347,7 +295,7 @@ export const productList = {
           {
             title: "Blendtec Stealth 885",
             link: "#",
-            imgSrc: "pic 27.png",
+            imgSrc: _pic27,
           },
         ],
       },
@@ -362,7 +310,7 @@ export const productList = {
           {
             title: "بكج الاسبريسو لمكاين E61 ",
             link: "https://espressostation.co/ar/%D8%A8%D8%A7%D9%82%D8%A9-%D8%A7%D9%84%D8%A7%D8%B3%D8%A8%D8%B1%D9%8A%D8%B3%D9%88-e61/p1649380971",
-            imgSrc: "pr.png",
+            imgSrc: _pr,
           },
         ],
       },
