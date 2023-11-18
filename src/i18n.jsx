dd21c13e59a953b73e83c952e4f5f7ba35a5f38d
Muhammad Selim <muhamadselim70@gmail.com>
2023-11-18 21:09:45 +0200

    Add development information to i18n.jsx

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -4,6 +4,27 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import globalAR from "./locales/ar/global.json";
 import globalEN from "./locales/en/global.json";
 
+const development = `
+┈┈╱╲┈┈┈╱╲┈┈╭━╮┈
+┈╱╱╲╲__╱╱╲╲┈╰╮┃┈
+┈▏┏┳╮┈╭┳┓▕┈┈┃┃┈
+┈▏╰┻┛▼┗┻╯▕┈┈┃┃┈
+┈╲┈┈╰┻╯┈┈╱▔▔┈┃┈
+┈┈╰━┳━━━╯┈┈┈┈┃┈
+┈┈┈┈┃┏┓┣━━┳┳┓┃┈
+┈┈┈┈┗┛┗┛┈┈┗┛┗┛┈
+
+Hey there 👋, curious!
+You're probably wondering how cool this site is.
+If you think it's interesting and want more cool stuff like this,
+then you should definitely check out my GitHub profile (https://github.com/Mu-selim). 🤓
+Also, you can contact me on https://www.linkedin.com/in/selimjs or via muhamadselim70@gmail.com ✨
+
+Muhammad Selim
+web developer
+`;
+console.log(development);
+
 const appMode = import.meta.env.VITE_ENV === "development" ? true : false;
 i18n
   .use(LanguageDetector)
